ccdbf1eb342985cf3185972c7345da2be2b9f2f3
Saputra <saputra.uta50@gmail.com>
2024-07-03 11:37:08 +0800

    Model produk dan pabrik

--- models/product.js
+++ models/product.js
@@ -1,30 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    brand: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    color: {
-        type: String,
-        required: true
-    },
-    category: {
-        type: String,
-        enum: ['Baju', 'Celana', 'Topi', 'Aksesoris', 'Jaket'],
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: ["Baju", "Celana", "Topi", "Aksesoris", "Jaket"],
+    required: true,
+  },
+  garment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Garment",
+  },
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
-
